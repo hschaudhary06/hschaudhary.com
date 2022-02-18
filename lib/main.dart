ff27cbebe7b3a7127590b4d3c5bb938240ef3871
hs chaudhary <hschaudhary06@gmail.com>
2022-02-18 21:57:28 +0530

    Day-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:drower/routes/MyRoutes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 import 'home.dart';
 import 'login.dart';
 import 'widgets/theme.dart';
@@ -8,8 +12,41 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var response = await http.get(
+            Uri.http(
+              "hschaudhary.epizy.com",
+              "api/api.php"
+            ),
+            headers:{"action":"getdata"}
+          );
+    var json = jsonDecode(response.body);
+    
+    List<User> users = [];
+
+    for(var u in json ){
+     User user = User(u["id"],u["username"],u["password"]);
+     users.add(user);
+    }
+
+    print(users.length);
+    return users;
+    // var Json = await rootBundle.loadString("assets/files/catelog.json");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +55,17 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: AppTheme.lightTheme(context),
       darkTheme: AppTheme.darkTheme(context),
-      initialRoute: MyRoutes.login,
+      initialRoute: MyRoutes.home,
       routes: {
         MyRoutes.login : (context) => Login(),
         MyRoutes.home : (context) => HomePage(),
       },
     );
   }
+}
+
+class User{
+  final String id,username,password;
+
+  User(this.id, this.username, this.password);
 }
